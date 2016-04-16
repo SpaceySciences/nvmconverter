7d3327e23b0a999cda515aad0d7617660cebb2ef
awkbr549 <adamking.br549@gmail.com>
2016-04-15 22:11:50 -0400

    now can completely read through all models and parse all info exactly

--- convert.py
+++ convert.py
@@ -52,10 +52,14 @@ outputFile = ""
 
 #argument bool list goes here
 verbose = False
+testing = False
 
 #variables for parsing nvm
 numCamerasTotal = 0
+numPointsTotal = 0
 numFullModels = 0
+numEmptyModels = 0
+numModelsTotal = 0
 
 #sets to be parsed into
 cameraFileListImages = [] # <File Name> --> one string
@@ -65,6 +69,10 @@ cameraFileListCameraCenter = [] # <camera center> --> three floats
 cameraFileListRadialDistortion = [] # <radial distortion> --> one int
 #there is a zero after each camera
 
+pointsXYZList = [] # <XYZ> --> three floats
+pointsRGBList = [] # <RBG> --> three ints
+pointsMeasurementsList = [] # multiple { <Image index> <Feature Index> <xy> } per item here
+
 #accept arguments
 if (len(sys.argv) == 2): # no argument, no specified output
 	inputFile = sys.argv[1]
@@ -72,15 +80,20 @@ elif (len(sys.argv) == 3): # either with argument or specified output, not both
 	if (sys.argv[1] == "-v"): # yes verbose, no specified output
 		verbose = True
 		inputFile = sys.argv[2]
+	elif (sys.argv[1] == "--testing"): # yes testing, no specified output
+		testing = True
+		inputFile = sys.argv[2]
 	else: # no argument, yes specified output
 		inputFile = sys.argv[1]
-		inputFile = sys.argv[2]
+		outputFile = sys.argv[2]
 elif (len(sys.argv) == 4): # expected ONE argument AND specified output	
-	if (sys.arv[1] == "-v"): # yes verbose
+	if (sys.argv[1] == "-v"): # yes verbose
 		verbose = True
-	inputFile = sys.arg[2]
-	outputFile = sys.arg[3]
-else: # no correct input
+	elif (sys.argv[1] == "--testing"): #yes testingg
+		testing = True
+	inputFile = sys.argv[2]
+	outputFile = sys.argv[3]
+else: # no correct input syntax
 	print "USAGE: \n> ./convert.py <-arg> <input>.nvm\n> ./convert.py <-arg> <input>.nvm <output>.json"
 	input()
 	sys.exit(0)
@@ -100,7 +113,7 @@ with open(inputFile) as f:
 
 	#read in version (don't know how version will change input yet)
 	nvmVersion = line[0:line.find(' ')] #getting just the version, no calibration
-	print "nvmVersion is: " + nvmVersion
+	if testing: print "nvmVersion is: " + nvmVersion
 
 	#version can be followed by calibration
 		#calibration command in file reads `FixedK fx cx fy cy'
@@ -108,82 +121,165 @@ with open(inputFile) as f:
 		nvmCalibration = line[line.find(' '):line.find('#')] 
 	else:
 		nvmCalibration = line[line.find(' '):len(line)]
-	print "nvmCalibration is: " + nvmCalibration
+	if testing: print "nvmCalibration is: " + nvmCalibration
 
-	#read through any blank or comment lines
+	#loop through models
 	while True:
-		line = f.readline()
-		line = line.rstrip()
-		if (not len(line) == 0 and not line.startswith('#')):
-			break	
+		#read through any blank or comment lines
+		while True:
+			line = f.readline()
+			line = line.rstrip()
+			if (not len(line) == 0 and not line.startswith('#')):
+				break	
+		#stop reading models if input is just "0"
+		if line[0] == "0":
+			break
+		numModelsTotal += 1
+		#read in full (have 3d points) and empty (no 3d points) models
+			#read in number of cameras
+		numCameras = int(line[0:])
+		numCamerasTotal += numCameras
+		if testing: print "numCameras is: " + str(numCameras)
+			#read in list of cameras
+		x = 0
+		while x < numCameras: # reading in however many cameras are in this model
+			line = f.readline()
+			line = line.rstrip()
+			if (len(line) == 0 or line.startswith('#')): # skipping any blank or commented lines
+				continue
+				#read in file name
+			if testing: print "remaining line is: " + line
+			cameraFileListImages.append(line[0:line.find(' ')]) # get each camera file location and store it
+			if testing: print "cameraFileListImages[" + str(x) + "] is: " + cameraFileListImages[x]
+			line = line[line.find(' ')+1:] # removing filename from temp reading line
+			line = line.strip()
+			if testing: print "remaining line is: " + line
+				#read in camera attributes
+				 	#read in focal length
+			cameraFileListFocalLength.append(line[0:line.find(' ')]) # <focal length> --> one integer
+			if testing: print "cameraFileListFocalLength[" + str(x) + "] is: " + cameraFileListFocalLength[x]
+			line = line[line.find(' ')+1:] # removing focal length from temp reading line
+			if testing: print "remaining line is: " + line
+			 		#read in quaternion <WXYZ>
+			quatWXYZ_list = ["", "", "", " "] # <quaternion <WXYZ> --> four floats
+			y = 0
+			while y < 4:
+				quatWXYZ_list[y] = line[0:line.find(' ')]
+				if testing: print "quatWXYZ_list[" + str(y) + "] is: " + quatWXYZ_list[y]
+				line = line[line.find(' ')+1:]
+				if testing: print "remaining line is: " + line
+				y += 1
+			cameraFileListQuaternionWXYZ.append(quatWXYZ_list)
+			 	#read in camera center <XYZ>
+			centerXYZ_list = ["", "", ""] # <camera center> --> three floats
+			y = 0
+			while y < 3:
+			 	centerXYZ_list[y] = line[0:line.find(' ')]
+			 	if testing: print "centerXYZ_list[" + str(y) + "] is: " + centerXYZ_list[y]
+			 	line = line[line.find(' ')+1:]
+			 	if testing: print "remaining line is: " + line
+			 	y += 1
+			cameraFileListCameraCenter.append(centerXYZ_list)
+			 	#read in radial distortion
+			cameraFileListRadialDistortion.append(line[0:line.find(' ')]) # <radial distortion> --> one int
+			if testing: print "cameraFileListRadialDistortion[" + str(x) + "] is: " + cameraFileListRadialDistortion[x]
+			#z = raw_input()
+				#there is a zero after each camera
+			x += 1
+		#end of while x
 
-	#read in full (have 3d points) and empty (no 3d points) models
-		#read in number of cameras
-	numCameras = int(line[0:])
-	line = line[line.find(' ')+1:]
-	numCamerasTotal += numCameras
-	print "numCameras is: " + str(numCameras)
-		#read in list of cameras
-	x = 0
-	while x < numCameras: # reading in however many cameras are in this model
-		line = f.readline()
-		line = line.rstrip()
-			#read in file name
-		print "remaining line is: " + line
-		cameraFileListImages.append(line[0:line.find(' ')]) # get each camera file location and store it
-		print "cameraFileListImages[" + str(x) + "] is: " + cameraFileListImages[x]
-		line = line[line.find(' ')+1:] # removing filename from temp reading line
-		line = line.strip()
-		print "remaining line is: " + line
-		#input()
-			#read in camera attributes
-			 	#read in focal length
-		cameraFileListFocalLength.append(line[0:line.find(' ')]) # <focal length> --> one integer
-		print "cameraFileListFocalLength[" + str(x) + "] is: " + cameraFileListFocalLength[x]
-		line = line[line.find(' ')+1:] # removing focal length from temp reading line
-		print "remaining line is: " + line
-		#input()
-		 		#read in quaternion <WXYZ>
-		quatWXYZ_list = [" ", " ", " ", " "] # <quaternion <WXYZ> --> four floats
-		y = 0
-		while y < 4:
-			quatWXYZ_list[y] = line[0:line.find(' ')]
-			print "quatWXYZ_list[" + str(y) + "] is: " + quatWXYZ_list[y]
-			line = line[line.find(' ')+1:]
-			print "remaining line is: " + line
-			y += 1
-		input() 
-		# 	#read in camera center <XYZ>
-		# cameraFileListQuaternionWXYZ.append(quatWXYZ_list)
-		# centerXYZ_list = ["", "", ""] # <camera center> --> three floats
-		# for y in range(0:3):
-		# 	centerXYZ_list[y] = line[0:line.find(' ')]
-		# 	print "centerXYZ_list[" + y + "] is: " + centerXYZ_list[]
-		# 	line = line[line.find(' ')+1:]
-		# 	print "remaining list is: " + line
-		# cameraFileListCameraCenter.append(centerXYZ_list)
-		# 	#read in radial distortion
-		# cameraFileListRadialDistortion.append(line[0:line.find(' ')]) # <radial distortion> --> one int
-		# print "cameraFileListRadialDistortion[" + x + "] is: " cameraFileListRadialDistortion[x]
-		# 	#there is a zero after each camera
-		x += 1
-	#end of while x
-
-
-		#read in number of 3d points
-		#read in 3d points
-			#read in point attributes
+		#read through any blank or comment lines
+		while True:
+			line = f.readline()
+			line = line.rstrip()
+			if (not len(line) == 0 and not line.startswith('#')):
+				break	
+		if testing: print "remaining line is: " + line
+			#read in number of 3d points
+		numPoints = int(line[0:])
+		numPointsTotal += numPoints
+		if numPoints > 0:
+			numFullModels += 1
+		else:
+			numEmptyModels += 1
+		if testing: print "numPoints is: " + str(numPoints)
+			#read in 3d point attributes
+		x = 0
+		while x < numPoints: # reading in however many cameras are in this model
+			line = f.readline()
+			line = line.rstrip()
+			if (len(line) == 0 or line.startswith('#')): # skipping any blank or commented lines
+				continue
 				#read in <XYZ>
-				#read in <RBG>
+			if testing: print "remaining line is: " + line
+			xyz_list = ["", "", ""]
+			y = 0
+			while y < 3:
+				xyz_list[y] = line[0:line.find(' ')]
+				if testing: print "xyz_list[" + str(y) + "] is: " + xyz_list[y]
+				line = line[line.find(' ')+1:]
+				line = line.strip()
+				if testing: print "remaining line is: " + line
+				y += 1
+			pointsXYZList.append(xyz_list)
+				#read in <RGB>
+			rgb_list = ["", "", ""]
+			y = 0
+			while y < 3:
+				rgb_list[y] = line[0:line.find(' ')]
+				if testing: print "rgb_list[" + str(y) + "] is: " + rgb_list[y]
+				line = line[line.find(' ')+1:]
+				line = line.strip()
+				if testing: print "remaining line is: " + line
+				y += 1
+			pointsRGBList.append(rgb_list)
 				#read in number of measurements
-				#read in list of measurements
+			measurements_list = [] # one of the contains many measurment_obj's
+			numMeasurements = int(line[0:line.find(' ')])
+			if testing: print "numMeasurements is: " + str(numMeasurements)
+			line = line[line.find(' ')+1:]
+			if testing: print "remaining line is: " + line
+			y = 0
+				#read in list of measurments
+			while y < numMeasurements:
 					#read in image index
+				measurement_obj = [] # --> <Image index> <Feature Index> <xy>
+				measurement_obj.append(line[0:line.find(' ')])
+				if testing: print "image index is: " + measurement_obj[0]
+				line = line[line.find(' ')+1:]
+				line = line.strip()
+				if testing: print "remainnig line is: " + line
 					#read in feature index
+				measurement_obj.append(line[0:line.find(' ')])
+				if testing: print "feature index is: " + measurement_obj[1]
+				line = line[line.find(' ')+1:]
+				line = line.strip()
+				if testing: print "remaining line is: " + line
+				#z = raw_input()
 					#read in <XY>
-
-
-	#read in 0 to stop reading in models
-
+				xy_list = ["", ""]
+				z = 0
+				while z < 2:
+					if z == 0: #not the last number in the line
+						xy_list[z] = line[0:line.find(' ')]
+					else: #yes the last number in the line
+						xy_list[z] = line[0:]
+					if testing: print "xy_list[" + str(z) + "] is: " + xy_list[z]
+					line = line[line.find(' ')+1:]
+					line = line.strip()
+					if testing: print "remaining line is: " + line
+					z += 1
+				#end of whil reading through <xy>
+			 	measurement_obj.append(xy_list)
+			 	measurements_list.append(measurement_obj)
+			 	y += 1
+			#end of while reading through measurements
+			pointsMeasurementsList.append(measurements_list)
+			x += 1
+		#end of while reading through points
+		#z = raw_input("\nFinished reading through a model...\n")
+	#end of while reading through models
+	z = raw_input("\nFinished reading through ALL models!\n")
 	#read in comments for the PLY section
 
 	#read in int for number of PLY files
